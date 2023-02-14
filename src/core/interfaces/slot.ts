4250ef9a7ee4504ec495050ccbcf6c15b1f470f6
Mari Spirandelli <mariannaspirandelli@gmail.com>
2023-02-14 15:22:55 -0300

    getting available slot implementation

--- src/core/interfaces/slot.ts
+++ src/core/interfaces/slot.ts
@@ -8,4 +8,5 @@ export interface ISlotController {
   create: (slot: SlotInput[]) => Promise<ISlot[]>;
   update: (id: number, slot: SlotInput) => Promise<ISlot[]>;
   search: (filter?: ISlotFilter) => Promise<ISlot[]>;
+  getAvailable: (vehicleTypeId: number) => Promise<ISlot[]>;
 }
--- src/core/slot.ts
+++ src/core/slot.ts
@@ -1,16 +1,26 @@
+import parkingSlotFactory from '../business/factories/parkingSlotFactory';
 import {
+  ISlot,
   ISlotFilter,
   SlotInput,
 } from '../domain/models/interfaces/iSlot';
 import { ISlotRepository } from '../domain/repositories/interfaces/iSlotRepository';
 import slotRepo from '../domain/repositories/slot';
 import { ISlotController } from './interfaces/slot';
+import { IVehicleTypeController } from './interfaces/vehicleType';
+import vehicleTypeController from './vehicleType';
 
-class SlotController implements ISlotController{
+class SlotController implements ISlotController {
   slotRepo: ISlotRepository;
 
-  constructor(slotRepo: ISlotRepository) {
+  vehicleTypeControl: IVehicleTypeController;
+
+  constructor(
+    slotRepo: ISlotRepository,
+    vehicleTypeControl: IVehicleTypeController
+  ) {
     this.slotRepo = slotRepo;
+    this.vehicleTypeControl = vehicleTypeControl;
   }
 
   create(slot: SlotInput[]) {
@@ -24,7 +34,21 @@ class SlotController implements ISlotController{
   update(id: number, slot: SlotInput) {
     return this.slotRepo.update(id, slot);
   }
+
+  async getAvailable(vehicleTypeId: number): Promise<ISlot[]> {
+    const vehicleTypes = await this.vehicleTypeControl.search();
+
+    const vehicleType = vehicleTypes.filter(
+      (item) => item.id === vehicleTypeId
+    )[0];
+
+    const parkingSlot = parkingSlotFactory.getParkingSlotByVehicleType(
+      vehicleType.type,
+    );
+
+    return parkingSlot.defineSlotToPark(vehicleTypes);
+  }
 }
 
-const slotController = new SlotController(slotRepo);
+const slotController = new SlotController(slotRepo, vehicleTypeController);
 export default slotController;
