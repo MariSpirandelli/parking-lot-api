9e4503dd0625a565713dce5f45e75c5b670f35f4
Mari Spirandelli <mariannaspirandelli@gmail.com>
2023-02-15 11:37:17 -0300

    vehicles api services

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import express from 'express';
 import parkingRouter from './parking';
 import parkingLotRouter from './parkingLot';
 import slotRouter from './slot';
+import vehicleRouter from './vehicle';
 
 const router = express.Router();
 
 router.use('/parkings', parkingRouter);
 router.use('/parking-lots', parkingLotRouter);
 router.use('/slots', slotRouter);
+router.use('/vehicles', vehicleRouter);
 
 export = router;
---
+++ src/routes/vehicle.ts
@@ -0,0 +1,45 @@
+import { Response, Request, Router } from 'express';
+import vehicleController from '../core/vehicle';
+import { BadRequestError } from '../infrastructure/express/errors';
+import { asyncHandler } from '../infrastructure/express/middlewares/errorHandler';
+
+const router = Router();
+
+router.post(
+  '/',
+  asyncHandler(async (req: Request, res: Response) => {
+    const plate = req.body.plate;
+    const vehicleTypeId = parseInt(req.body.vehicleTypeId, 10);
+
+    if (isNaN(vehicleTypeId)) {
+      throw new BadRequestError(
+        'Vehicle id are mandatory and must be a valid value'
+      );
+    }
+
+    return res
+      .status(200)
+      .json(await vehicleController.create({ plate, vehicleTypeId }));
+  })
+);
+
+router.put(
+  '/:vehicleId',
+  asyncHandler(async (req: Request, res: Response) => {
+    const vehicleId = parseInt(req.params.vehicleId, 10);
+
+    if (isNaN(vehicleId)) {
+      throw new BadRequestError('Missing vehicle id');
+    }
+
+    const { plate, vehicleTypeId } = req.body;
+
+    return res
+      .status(200)
+      .json(
+        await vehicleController.update(vehicleId, { plate, vehicleTypeId })
+      );
+  })
+);
+
+export = router;
