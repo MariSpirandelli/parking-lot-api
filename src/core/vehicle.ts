9b9a900a6e3186b07b7d14feed32fd1a5380582f
Mari Spirandelli <mariannaspirandelli@gmail.com>
2023-02-16 09:54:32 -0300

    bug fix - should not create vehicle if it already exists

--- src/core/vehicle.ts
+++ src/core/vehicle.ts
@@ -1,20 +1,23 @@
-import {
-  IVehicleFilter,
-  VehicleInput,
-} from '../domain/models/interfaces/iVehicle';
+import { IVehicleFilter, VehicleInput } from '../domain/models/interfaces/iVehicle';
 import { IVehicleRepository } from '../domain/repositories/interfaces/iVehicleRepository';
 import vehicleRepo from '../domain/repositories/vehicle';
 import { IVehicleController } from './interfaces/vehicle';
 
-class VehicleController implements IVehicleController{
+class VehicleController implements IVehicleController {
   vehicleRepo: IVehicleRepository;
 
   constructor(vehicleRepo: IVehicleRepository) {
     this.vehicleRepo = vehicleRepo;
   }
 
-  create(vehicle: VehicleInput) {
-    return this.vehicleRepo.persist(vehicle);
+  async create(vehicleInput: VehicleInput) {
+    const vehicle = await this.vehicleRepo.fetch({ plate: vehicleInput.plate });
+
+    if (vehicle) {
+      return vehicle;
+    }
+
+    return this.vehicleRepo.persist(vehicleInput);
   }
 
   search(filter?: IVehicleFilter) {
